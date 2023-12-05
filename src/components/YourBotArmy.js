6f6652983e806ec2d2892440da7dee4b2ac91795
Michael <muniu.dev@gmail.com>
2023-12-05 08:49:07 +0300

    x button fixed and functioning

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -9,7 +9,7 @@ function YourBotArmy({ bots, removeBot }) {
         key={bot.id}
         bot={bot}
             clickEvent={removeBot}
-        
+            hideBot={removeBot}
       />
     );
   });
